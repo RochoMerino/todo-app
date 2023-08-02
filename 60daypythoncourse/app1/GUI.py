15ecc6ecc00f7148c8360473eb584ddcb12f5d5d
rocho <rochodelaparra@gmail.com>
2023-08-02 00:05:35 -0600

    implementation of edit button

--- 60daypythoncourse/app1/GUI.py
+++ 60daypythoncourse/app1/GUI.py
@@ -5,10 +5,19 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do here", key="todo")
 add_button = sg.Button("Add", tooltip="Add to-do to the list")
 exit_button = sg.Button("Exit", tooltip="Click-here if you want to exit")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit", tooltip="Click-here if you want to edit a to-do")
+
 
 window = sg.Window('To-do application',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button],
+                           [exit_button]],
+
                    font=('Helvetica', 20))
+
 while True:
     event, values = window.read()
     print(event)
@@ -20,8 +29,24 @@ while True:
             new_todos = values['todo'] + "\n"
             todos.append(new_todos)
             functions.write_todos(todolist_arg=todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
 
         case sg.WIN_CLOSED:
             break
+
+
 
 window.close()
